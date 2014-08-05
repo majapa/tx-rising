f1d9ea8da2e086d5d512ce47b4330a758dd6bc87
majapa <jake@tfn.org>
2014-08-05 11:07:40 -0500

    first load

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+// you can override default options globally, so they apply to every .expander() call
+$.expander.defaults.slicePoint = 120;
+
+$(document).ready(function() {
+  // simple example, using all default options unless overridden globally
+  $('div.expand').expander();
+
+});
